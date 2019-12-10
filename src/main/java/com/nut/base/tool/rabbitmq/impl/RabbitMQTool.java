c5ebc5af7b0f59f8a7c38e0c4c50fcc4e762af28
Nuthhh <hjg9704@163.com>
2019-12-10 22:20:38 +0800

    【新增功能】增加RabbitMQTool相关

--- src/main/java/com/nut/base/tool/rabbitmq/impl/RabbitMQTool.java
+++ src/main/java/com/nut/base/tool/rabbitmq/impl/RabbitMQTool.java
@@ -1,9 +1,13 @@
 package com.nut.base.tool.rabbitmq.impl;
 
 import com.nut.base.tool.rabbitmq.IRabbitMQTool;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+
+import java.io.IOException;
 
 /**
  * @Auther: han jianguo
@@ -24,9 +28,36 @@ public class RabbitMQTool implements IRabbitMQTool {
         connectionFactory.setPassword(password);
     }
 
+    public RabbitMQTool(String host,int port,String username,String password,String virtualHost){
+        connectionFactory = new ConnectionFactory();
+        connectionFactory.setHost(host);
+        connectionFactory.setPort(port);
+        connectionFactory.setUsername(username);
+        connectionFactory.setPassword(password);
+        connectionFactory.setVirtualHost(virtualHost);
+    }
 
     @Override
     public boolean isConnection() {
-        return false;
+        Connection connection = null;
+        Channel channel = null;
+        try {
+            connection = connectionFactory.newConnection();
+            channel = connection.createChannel();
+        } catch (Exception e) {
+            log.error("RabbitMQ服务错误");
+            return false;
+        } finally {
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return true;
     }
+
+
 }
